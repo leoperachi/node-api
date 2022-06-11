3b71b76ddb57f180e06d40650dae64aeaf5fae55
Leonardo Perachi <leoperachi@gmail.com>
2022-06-11 11:20:46 -0300

    try fix erro mongo

--- db.js
+++ db.js
@@ -1,7 +1,14 @@
 var mongoose = require('mongoose');
 var config = require('./config');
 
-mongoose.connect(config.db, { useNewUrlParser: true });
+mongoose.connect(config.db, { useNewUrlParser: true }).then( x => 
+    {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    }).catch(err => 
+    {
+        console.error("Error connecting to mongo", err);
+    }
+);
 
 module.exports = 
 { 
